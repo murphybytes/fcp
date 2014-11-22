87f01dcfe2cb2d273c4c0f65eec8e2ebc46e6c4d
John Murphy <murphybytes@gmail.com>
2014-11-22 17:05:45 +0800

    more command line flags

---
+++ constants.go
@@ -0,0 +1,5 @@
+package main
+
+// process return codes
+const errorGeneric        = 1
+const errorImproperUsage  = 2
--- fcp.go
+++ fcp.go
@@ -3,10 +3,11 @@ package main
 import (
   "fmt"
   "flag"
+  "os"
   )
 
   type Arguments struct {
-    deamonize *bool
+    daemonize *bool
     server    *bool
 
   }
@@ -14,10 +15,22 @@ import (
   func main() {
 
     arguments := new(Arguments)
-    arguments.deamonize = flag.Bool("d", false, "Run server as a daemon")
+    arguments.daemonize = flag.Bool("d", false, "Run server as a daemon")
     arguments.server    = flag.Bool("s", false, "Run server in foreground")
-    
+
+
     flag.Parse()
 
-    fmt.Println( *arguments.deamonize )
+    if *arguments.daemonize || *arguments.server {
+      // fire up in server mode
+    } else {
+      // if we are here we are in interactive mode, look for file source and
+      // destination as last two arguments
+      fileArgs := flag.Args()
+      if len(fileArgs) != 2 {
+        os.Exit( errorImproperUsage )
+      }
+    }
+
+    fmt.Println( *arguments.daemonize )
   }
