bef1902365550b11be262da8469f026f04d82954
John Murphy <murphybytes@gmail.com>
2014-11-24 18:05:04 +0800

    got override of flags output working

--- fcp.go
+++ fcp.go
@@ -18,16 +18,23 @@ import (
     arguments.daemonize = flag.Bool("d", false, "Run server as a daemon")
     arguments.server    = flag.Bool("s", false, "Run server in foreground")
 
-
+   flag.Usage = func() {
+     fmt.Println("Usage: fcp [options] [[[user1][@host1]]:sourcefile [[user2][@host2]]:destfile]")
+     fmt.Println("Options:")
+     flag.PrintDefaults()
+   }
     flag.Parse()
 
     if *arguments.daemonize || *arguments.server {
+      fmt.Println("xxx")
       // fire up in server mode
     } else {
       // if we are here we are in interactive mode, look for file source and
       // destination as last two arguments
       fileArgs := flag.Args()
       if len(fileArgs) != 2 {
+        flag.Usage()
+
         os.Exit( errorImproperUsage )
       }
     }
